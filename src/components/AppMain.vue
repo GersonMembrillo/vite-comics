33c53dee40baf8c65c8affa7a81333f61508361a
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-04-21 16:00:38 +0200

    add load more button (work in progress)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,22 @@
 <template>
-<div class="main-top py-3 bg-dark">
+<div class="main-top py-3 bg-dark position-relative">
   <div class="container">
     <div class="row">
       <CardComponent :image="card.thumb" :title="card.series" v-for="(card,index) in cards" :key="index" />
     </div>
   </div>
+  <div class="d-flex justify-content-center">
+    <button type="button" class="btn btn-primary d-flex rounded-0">LOAD MORE</button>
+  </div>
+
 </div>
 <div class="main-bottom p-5">
   <div class="container">
-    <ul class="d-flex list-unstyled row-cols-5">
+    <ul class="d-flex list-unstyled row-cols-5 mb-0">
       <li class="" v-for="(button, index) in buttons" :key="index">
-        <a class="text-decoration-none text-white d-flex" :href="button.url" :class="{ 'active': button.current }">
-          <img :src="button.src" :alt="button.name">
-          {{ button.name }}
+        <a class="text-decoration-none text-white d-flex justify-content-center align-items-center" :href="button.url" :class="{ 'active': button.current }">
+          <img class="bottom-img pe-2" :src="button.src" :alt="button.name">
+          <p class="mb-0 text-uppercase">{{ button.name }}</p>
         </a>
       </li>
     </ul>
@@ -44,5 +48,9 @@ export default {
 <style lang="scss" scoped>
 .main-bottom{
   background-color: #0082F9;
+  .bottom-img{
+    width: auto;
+    height: 50px;
+  }
 }
 </style>
